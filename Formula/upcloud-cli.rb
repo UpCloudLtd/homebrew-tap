0ee34b46b3375fe4d2980635b475a88f56e5ccd9
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-30 10:57:26 +0200

    Brew formula update for upcloud-cli version v1.1.1

--- Formula/upcloud-cli.rb
+++ Formula/upcloud-cli.rb
@@ -5,29 +5,29 @@
 class UpcloudCli < Formula
   desc "CLI tool for managing UpCloud services."
   homepage "https://upcloud.com/"
-  version "1.1.0"
+  version "1.1.1"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/UpCloudLtd/upcloud-cli/releases/download/v1.1.0/upcloud-cli_1.1.0_darwin_x86_64.tar.gz"
-      sha256 "e662c9bb8f515acc477c6f9424a0e759cd871915a3d018b74a61daae2e60ae35"
+      url "https://github.com/UpCloudLtd/upcloud-cli/releases/download/v1.1.1/upcloud-cli_1.1.1_darwin_x86_64.tar.gz"
+      sha256 "e45a0f833158a617f3ef7477bc791f513fc86a016f781ada87af4bc742c8955a"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/UpCloudLtd/upcloud-cli/releases/download/v1.1.0/upcloud-cli_1.1.0_darwin_arm64.tar.gz"
-      sha256 "b4675cd06c2dd7757d96530bb5dcae39c0fd91071d3fdcc8240bbe9cf86626de"
+      url "https://github.com/UpCloudLtd/upcloud-cli/releases/download/v1.1.1/upcloud-cli_1.1.1_darwin_arm64.tar.gz"
+      sha256 "76ee645bba7336215003bcbd12a98f2f8e315fc8fc37e63af6138c23e8af6de9"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/UpCloudLtd/upcloud-cli/releases/download/v1.1.0/upcloud-cli_1.1.0_linux_x86_64.tar.gz"
-      sha256 "78b8acdf27d6b1c306b9f364645b742f7810fddcc9301ba76db23be9414b5f5e"
+      url "https://github.com/UpCloudLtd/upcloud-cli/releases/download/v1.1.1/upcloud-cli_1.1.1_linux_x86_64.tar.gz"
+      sha256 "e162764fae790fe1820f7a7ee3db1d2bfe54568cbed5272831c05161c6763ce6"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/UpCloudLtd/upcloud-cli/releases/download/v1.1.0/upcloud-cli_1.1.0_linux_arm64.tar.gz"
-      sha256 "1ad3390f7be516942ebfc280af8573204288070085e8343c0a562e89d0dbdc9f"
+      url "https://github.com/UpCloudLtd/upcloud-cli/releases/download/v1.1.1/upcloud-cli_1.1.1_linux_arm64.tar.gz"
+      sha256 "3f5043bf66402a0e0cadbd48c608be6bd12c9befb53f37493afd0d6d61e7b095"
     end
   end
 
